b2d28638dfde939e233e5b363d43c8f5c6cd0c61
Rahul Sharma <holdmypotion@gmail.com>
2021-07-27 08:32:44 +0530

    Completed

---
+++ src/components/about.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import styles from '../styles/about.module.css';
+
+export default function About() {
+  return (
+    <div className={styles.container}>
+      <h1>About me</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste nostrum
+        eius sapiente veritatis? Veritatis quasi iusto sed repellendus, ea cum
+        atque dicta deserunt nam nesciunt accusantium illo numquam aliquid
+        provident pariatur et distinctio ipsum mollitia quos tempore aspernatur
+        laborum amet adipisci incidunt! Distinctio nisi sed, dolores minus
+        officia dolor? Esse.
+      </p>
+    </div>
+  );
+}
