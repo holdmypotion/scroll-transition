20dfeaffd5bb65be5afaa50a831f79096d961d09
Rahul Sharma <holdmypotion@gmail.com>
2021-08-17 17:52:32 +0530

    Removed Smooth scroll

--- src/components/heroSection.js
+++ src/components/heroSection.js
@@ -1,39 +1,35 @@
 import React from 'react';
-import {
-  motion,
-  useViewportScroll,
-  useTransform,
-  useSpring,
-} from 'framer-motion';
+// 1.
+import { motion, useViewportScroll, useTransform } from 'framer-motion';
 
 import styles from '../styles/heroSection.module.css';
 import heroImage from '../assets/images/5.jpeg';
 
-export default function HeroSection({ offset }) {
-  // const physics = { damping: 200, stiffness: 2000 };
-  const physics = { damping: 15, mass: 0.27, stiffness: 95 };
+export default function HeroSection({ offset = 1500 }) {
+  // 2.
   const { scrollY } = useViewportScroll();
+  // 3.
   const scale = useTransform(scrollY, [0, offset], [1, 5]);
-  const scaleSmooth = useSpring(scale, physics);
   const opacity = useTransform(scrollY, [0, offset], [3, 0]);
-  const opacitySmooth = useSpring(opacity, physics);
   const moveDown = useTransform(scrollY, [0, offset], [0, -1200]);
-  const moveDownSmooth = useSpring(moveDown, physics);
+
   return (
     <>
       <div className={styles.imageContainer}>
+        {/* 5. */}
         <motion.img
           src={heroImage}
           alt='Model'
           style={{
-            opacity: opacitySmooth,
-            scale: scaleSmooth,
-            y: moveDownSmooth,
+            opacity: opacity,
+            scale: scale,
+            y: moveDown,
           }}
         />
       </div>
+      {/* 6. */}
       <div style={{ background: '#030303', height: `${offset}px` }}></div>
-      <div style={{ background: '#030303', height: '100vh' }}></div>
+      <div style={{ background: '#030303', height: '80vh' }}></div>
     </>
   );
 }
