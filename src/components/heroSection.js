6757e9fb0a7a9a485bd60b5efa65b6d118ba9abf
Rahul Sharma <holdmypotion@gmail.com>
2021-09-01 16:58:08 +0530

    Updated the herosection.js

--- src/components/heroSection.js
+++ src/components/heroSection.js
@@ -11,7 +11,7 @@ export default function HeroSection({ offset = 1500 }) {
   // 3.
   const scale = useTransform(scrollY, [0, offset], [1, 5]);
   const opacity = useTransform(scrollY, [0, offset], [3, 0]);
-  const moveDown = useTransform(scrollY, [0, offset], [0, -1200]);
+  const moveDown = useTransform(scrollY, [0, offset], [0, -1000]);
 
   return (
     <>
